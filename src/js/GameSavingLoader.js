ced70f0d5145a5b01907c62380391c6bcc39dfcd
Polina <sgsey75@gmail.com>
2021-12-03 18:52:53 +0300

    correction Promises

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,11 +1,3 @@
-// import read from './reader';
-// import json from './parser';
-
-// export default class GameSavingLoader {
-//   static load() {
-//     return read().then((data) => json(data));
-//   }
-// }
 import GameSaving from './GameSaving';
 import read from './reader';
 import json from './parser';
